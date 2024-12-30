b349b81e7c0406f23598550608d78265de6e29a6
Вячеслав <slavvva05@gmail.com>
2024-12-30 21:38:27 +0300

    Add Dto files

---
+++ Приложение/App/Servicelayer/DTOs/CommentSendDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicelayer.Dtos
+{
+    public class CommentSendDTO
+    {
+       
+        public int EmployeeId { get; set; }
+        public string Comment { get; set; } = null!;
+
+    }
+}
---
+++ Приложение/App/Servicelayer/DTOs/Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicelayer.Dtos
+{
+    public class Error
+    {
+        public string TimeStamp {  get; set; }
+        public string Message { get; set; }
+        public string ErrorCode { get; set; }
+    }
+}
