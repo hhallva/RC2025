72e046d69f540d04be257e9ae8191642c5dc8f6b
Вячеслав <slavvva05@gmail.com>
2025-01-03 19:10:58 +0300

    Delete EmployeeDTO, Error, MaterialDTO

--- Приложение/App/Servicelayer/DTOs/EmployeeDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServiceLayer.Dtos
-{
-    public class EmployeeDto
-    {
-        public string Password { get; set; } = null!;
-
-        public string Email { get; set; } = null!;
-    }
-}
