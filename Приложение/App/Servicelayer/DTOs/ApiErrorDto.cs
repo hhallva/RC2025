bdc88e709e3b7f6d4b26e3517123931f55035f40
Вячеслав <slavvva05@gmail.com>
2025-01-03 21:36:51 +0300

    Update ApiErrorDto.cs

--- Приложение/App/Servicelayer/DTOs/ApiErrorDto.cs
+++ Приложение/App/Servicelayer/DTOs/ApiErrorDto.cs
@@ -1,7 +1,20 @@
 ﻿namespace WebApi.DTOs
 {
-    public class ApiErrorDto(long Timmestamp, string Message, int ErrorCode)
+    public class ApiErrorDto
     {
+        public long Timestamp { get; }
+
+        public string Message { get; }
+
+        public int ErrorCode { get; }
+
+        public ApiErrorDto(long timestamp, string message, int errorCode)
+        {
+            Timestamp = timestamp;
+            Message = message;
+            ErrorCode = errorCode;
+        }
+
         public ApiErrorDto(string message, int errorCode) :
             this(DateTimeOffset.UtcNow.ToUnixTimeSeconds(), message, errorCode)
         {
