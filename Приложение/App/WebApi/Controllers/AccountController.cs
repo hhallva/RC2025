660166c63a326984de35accc509597c13cf1f23c
Вячеслав <slavvva05@gmail.com>
2025-01-03 21:37:55 +0300

    Added documentation and error handling

--- Приложение/App/WebApi/Controllers/AccountController.cs
+++ Приложение/App/WebApi/Controllers/AccountController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 using ServiceLayer.Data;
-using ServiceLayer.Dtos;
 using ServiceLayer.DTOs;
 using ServiceLayer.Services;
+using WebApi.DTOs;
 
 namespace WebApi.Controllers
 {
@@ -19,19 +20,32 @@ namespace WebApi.Controllers
             _context = context;
         }
 
+
+        /// <summary>
+        /// POST: /api/v1/SignIn
+        /// Вход в учетную запись, генерация JWT-токена
+        /// </summary>
+        /// <param name="employee">Объект содержащий email и пароль пользователя</param>
+        /// <returns>JWT-токен</returns>
+        /// <response code="200">Успешная авторизация</response>
+        /// <response code="400">Неверные параметры</response>
+        /// <response code="401">Не удалось авторизоваться</response>
         [HttpPost("SingIn")]
-        public IActionResult Login(EmployeeDto employee)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiErrorDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ApiErrorDto))]
+        public IActionResult Login(LoginDto employee)
         {
             if (string.IsNullOrWhiteSpace(employee.Email))
-                return BadRequest("Почта не указана");
+                return BadRequest(new ApiErrorDto("Почта не указана", 3000));
             if (string.IsNullOrWhiteSpace(employee.Password))
-                return BadRequest("Пароль не указан");
+                return BadRequest(new ApiErrorDto("Пароль не указан", 3001));
 
             var dbEmployee = _context.Employees.FirstOrDefault(u => u.Email == employee.Email);
             if (dbEmployee is null)
-                return NotFound("Пользователь не найден");
+                return NotFound(new ApiErrorDto("Пользователь не найден", 3002));
             if (dbEmployee.Password != employee.Password)
-                return BadRequest("Почта или пароль неверны");
+                return BadRequest(new ApiErrorDto("Почта или пароль неверны", 3003));
             return Ok(_tokenService.GenerateToken(dbEmployee));
         }
     }
--- Приложение/App/WebApi/Controllers/DocumentsController.cs
+++ Приложение/App/WebApi/Controllers/DocumentsController.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ServiceLayer.Data;
-using ServiceLayer.Dtos;
 using ServiceLayer.DTOs;
 using ServiceLayer.Models;
+using WebApi.DTOs;
 
 namespace WebApi.Controllers
 {
     [Route("api/v1/")]
     [ApiController]
+    [Authorize]
     public class DocumentsController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -18,75 +20,107 @@ namespace WebApi.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// GET: /api/v1/Documents
+        /// Получение списка всех документов
+        /// </summary>
+        /// <returns>Список объектов DocumentDto</returns>
+        /// <response code="200">Успешное получение списка</response>
+        /// <response code="404">Документы не найдены</response>
         [HttpGet("Documents")]
-        public async Task<ActionResult<IEnumerable<MaterialDto>>> GetDocumentsAsync()
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DocumentDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ApiErrorDto))]
         public async Task<ActionResult<IEnumerable<DocumentDto>>> GetDocumentsAsync()
         {
-            return await _context.Documents
-                .Select(m => new MaterialDto
+            var documents = await _context.Documents
                 .Select(m => new DocumentDto
                 {
                     Id = m.DocumentId,
                     Title = m.Name,
-                    CreateDate = m.CreateDate,
-                    ConfirmDate = m.ConfirmDate,
                     CreatedDate = m.CreateDate,
                     ApprovedDate = m.ConfirmDate,
                     Category = m.Category,
                     HasComments = m.Comments.Count != 0
                 }).ToListAsync();
+
+            if (!documents.Any())
+                return NotFound(new ApiErrorDto("Документы не найдены", 1001));
+            return documents;
         }
 
+        /// <summary>
+        /// GET: /api/v1/Document/{id}/Comments
+        /// Получение списка комментариев у конктерного документа
+        /// </summary>\
+        /// <param name="id">Id документа, у которого хотим получить комментарии</param>
+        /// <returns>Список комментариев типа CommentDto</returns>
+        /// <response code="200">Успешное получение списка</response>
+        /// <response code="400">Неправильный параметр</response>
+        /// <response code="404">Не найдены комментарии для документа</response>
         [HttpGet("Document/{id}/Comments")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CommentDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiErrorDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ApiErrorDto))]
         public async Task<ActionResult<IEnumerable<CommentDto>>> GetDocumentComments(int id)
         {
+            if (id <= 0)
+                return BadRequest(new ApiErrorDto("Недопустимый идентификатор документа. ID должен быть положительным числом.", 2000));
+
             var comments = await _context.Comments
-                .Include(c => c.Employee)
-                .ThenInclude(e => e.Position)
-                .Where(c => c.DocumentId == id)
-                .ToListAsync();
-            
+            .Include(c => c.Employee)
+            .ThenInclude(e => e.Position)
+            .Where(c => c.DocumentId == id)
+            .ToListAsync();
+
             var commentDtos = comments.Select(c => c.ToDto()).ToList();
 
+            if (!commentDtos.Any())
+                return NotFound(new ApiErrorDto("Не найдены комментарии для документа", 2001));
             return Ok(commentDtos);
         }
 
+        /// <summary>
+        /// POST: /api/v1/Document/{id}/Comments
+        /// Создание комментария для конктерного документа
+        /// </summary>
+        /// <param name="id">Id документа, которому хотим оставить комменатрий</param>
+        /// <param name="commentDto">Данные о комменатрие</param>
+        /// <returns>Статус создания и id созданного комментария</returns>
+        /// <response code="200">Успешное создание комментария</response>
+        /// <response code="400">Неправильные параметры</response>       
         [HttpPost("Document/{id}/Comments")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CommentDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiErrorDto))]
         public async Task<ActionResult<CommentDto>> PostComment(int id, CommentDto commentDto)
         {
-            var comment = new Comment
+            try
             {
-                DocumentId = id,
-                Text = commentDto.Text,
-                CreatedDate = DateTime.Now,
-                UpdatedDate = null,
-                EmployeeId = commentDto.Author.Id
-            };
+                var comment = new Comment
+                {
+                    DocumentId = id,
+                    Text = commentDto.Text,
+                    CreatedDate = DateTime.Now,
+                    UpdatedDate = null,
+                    EmployeeId = commentDto.Author.Id
+                };
 
-            _context.Comments.Add(comment);
-            await _context.SaveChangesAsync();
+                _context.Comments.Add(comment);
+                await _context.SaveChangesAsync();
 
-            await _context.Comments
-                .Include(c => c.Employee)
-                .ThenInclude(e => e.Position)
-                .Where(c => c.DocumentId == id)
-                .ToListAsync();
+                await _context.Comments
+                    .Include(c => c.Employee)
+                    .ThenInclude(e => e.Position)
+                    .Where(c => c.DocumentId == id)
+                    .ToListAsync();
 
-            commentDto = comment.ToDto();
+                commentDto = comment.ToDto();
 
-            return CreatedAtAction("GetComment", new { id = comment.CommentId }, commentDto);
-        }
-
-        [HttpGet("Comments/{id}")]
-        public async Task<ActionResult<CommentDto>> GetComment(int id)
-        {
-            var comment = await _context.Comments
-                .Include(c => c.Employee)
-                .ThenInclude(e => e.Position)
-                .FirstOrDefaultAsync(c => c.CommentId == id);
-
-            return Ok(comment?.ToDto());
-            return Created("", commentDto);
+                return Created("", commentDto);
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest(new ApiErrorDto("Ошибка при сохранении комментария", 2002));
+            }
         }
     }
 }
