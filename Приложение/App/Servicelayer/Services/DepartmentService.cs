2de4ad5f8fdd89cf9ecdc01d71abe76762dd4f55
Вячеслав <slavvva05@gmail.com>
2025-01-04 18:54:34 +0300

    Create DepartmentService.cs

---
+++ Приложение/App/Servicelayer/Services/DepartmentService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceLayer.Data;
+using ServiceLayer.Models;
+
+namespace ServiceLayer.Services
+{
+    public class DepartmentService(AppDbContext context)
+    {
+
+    }
+}
