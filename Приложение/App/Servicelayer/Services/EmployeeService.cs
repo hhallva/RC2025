8b70d149bbe430a5004d38d4db74157347a30c9f
Вячеслав <slavvva05@gmail.com>
2025-01-10 15:16:35 +0300

    Create EmployeeService.cs

---
+++ Приложение/App/Servicelayer/Services/EmployeeService.cs
@@ -0,0 +1,32 @@
+﻿using DataLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Services
+{
+    public class EmployeeService
+    {
+        private readonly HttpClient _client;
+        private readonly string _baseUrl = "http://localhost:5297/api/v1/Employees/";
+
+        public EmployeeService(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new(_baseUrl);
+        }
+
+        public async Task DismissAsync(int id) 
+            => await _client.PatchAsJsonAsync($"{id}", id);
+
+        public async Task UpdateAsync(Employee employee) 
+            => await _client.PutAsJsonAsync($"{employee.EmployeeId}", employee);
+
+        public async Task<Employee> GetAsync(int id) 
+            => await _client.GetFromJsonAsync<Employee>($"{id}");
+    }
+}
