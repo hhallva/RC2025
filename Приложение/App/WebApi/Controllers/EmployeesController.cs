582468cbe9f8f2adb923c37877d1ef368a543882
Вячеслав <slavvva05@gmail.com>
2025-01-10 15:16:30 +0300

    Update EmployeesController.cs

--- Приложение/App/WebApi/Controllers/EmployeesController.cs
+++ Приложение/App/WebApi/Controllers/EmployeesController.cs
@@ -7,98 +7,49 @@ namespace WebApi.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
-    public class EmployeesController : ControllerBase
+    public class EmployeesController(AppDbContext context) : ControllerBase
     {
-        private readonly AppDbContext _context;
-
-        public EmployeesController(AppDbContext context)
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> DismissEmployeeAsync(int id)
         {
-            _context = context;
-        }
-
-
-        // GET: api/Employees
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
-        {
-            return await _context.Employees.ToListAsync();
-        }
-
-        // GET: api/Employees/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Employee>> GetEmployee(int id)
-        {
-            var employee = await _context.Employees.FindAsync(id);
-
+            var employee = await context.Employees.FindAsync(id);
             if (employee == null)
-            {
                 return NotFound();
-            }
 
-            return employee;
+            employee.DismissalDate = DateOnly.FromDateTime(DateTime.Now);
+            context.Employees.Update(employee);
+            await context.SaveChangesAsync();
+            return Ok(employee);
         }
 
-        // PUT: api/Employees/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        public async Task<IActionResult> PutEmployeeAsync(int id, Employee employee)
         {
             if (id != employee.EmployeeId)
-            {
                 return BadRequest();
-            }
 
-            _context.Entry(employee).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!EmployeeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            context.Entry(employee).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(employee);
         }
 
-        // POST: api/Employees
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeAsync(int id)
         {
-            _context.Employees.Add(employee);
-            await _context.SaveChangesAsync();
+            var employee = await context.Employees.FindAsync(id);
 
-            return CreatedAtAction("GetEmployee", new { id = employee.EmployeeId }, employee);
-        }
-
-        // DELETE: api/Employees/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteEmployee(int id)
-        {
-            var employee = await _context.Employees.FindAsync(id);
             if (employee == null)
-            {
                 return NotFound();
-            }
 
-            _context.Employees.Remove(employee);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return Ok(employee);
         }
 
-        private bool EmployeeExists(int id)
+        [HttpPost]
+        public async Task<IActionResult> PostEmployeeAsync(Employee employee)
         {
-            return _context.Employees.Any(e => e.EmployeeId == id);
+            await context.Employees.AddAsync(employee);
+            await context.SaveChangesAsync();
+            return Ok(employee);
         }
     }
 }
