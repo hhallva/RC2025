f14fadb32344c76990cdaf3d157ffa262eb492f7
Вячеслав <slavvva05@gmail.com>
2025-01-10 15:24:24 +0300

    Update Employee.cs

--- Приложение/App/Servicelayer/Models/Employee.cs
+++ Приложение/App/Servicelayer/Models/Employee.cs
@@ -24,7 +24,7 @@ public partial class Employee
 
     public int? DirectManager { get; set; }
 
-    public DateOnly? Birthday { get; set; }
+    public DateTime? Birthday { get; set; }
 
     public string Password { get; set; } = null!;
 
