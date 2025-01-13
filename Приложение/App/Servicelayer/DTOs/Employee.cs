67f795f7b24dab50a38330cbeb9add25f762c246
Вячеслав <slavvva05@gmail.com>
2025-01-13 17:34:05 +0300

    Update DepartmentsWindow.xaml
    Add DataTrigger IsEnebled

--- Приложение/App/Servicelayer/DTOs/Employee.cs
+++ Приложение/App/Servicelayer/DTOs/Employee.cs
@@ -10,6 +10,7 @@ namespace DataLayer.Models
          null : (int)(DateTime.Now - DismissalDate.Value.ToDateTime(TimeOnly.MinValue)).TotalDays;
 
         public bool DismissedAgo => (DismissalDate != null && DayAfterDismissal <= 30) ? false : true;
+        public bool IsDismiss => (DismissalDate != null) ? true :  false;
     }
 }
 
