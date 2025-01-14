9347e018cb34eefdbeaa80c042aa487b603a3b07
Вячеслав <slavvva05@gmail.com>
2025-01-14 20:25:43 +0300

    Update if construction

--- Приложение/App/Servicelayer/DTOs/Employee.cs
+++ Приложение/App/Servicelayer/DTOs/Employee.cs
@@ -9,7 +9,7 @@ namespace DataLayer.Models
         public int? DayAfterDismissal => (DismissalDate == null) ?
          null : (int)(DateTime.Now - DismissalDate.Value.ToDateTime(TimeOnly.MinValue)).TotalDays;
 
-        public bool DismissedAgo => (DismissalDate != null && DayAfterDismissal <= 30) ? false : true;
+        public bool DismissedAgo => (DismissalDate != null && DayAfterDismissal <= 30) ? true : false;
         public bool IsDismiss => (DismissalDate != null) ? true :  false;
     }
 }
