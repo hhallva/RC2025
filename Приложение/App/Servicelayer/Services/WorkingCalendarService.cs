65efaa7d74d2e6e77b34c97b6d46d6e985993a97
Вячеслав <slavvva05@gmail.com>
2025-01-22 22:33:45 +0300

    Add WorkingCalendarController,  WorkingCalendarService

---
+++ Приложение/App/Servicelayer/Services/WorkingCalendarService.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Models;
+using System.Net.Http.Json;
+
+namespace DataLayer.Services
+{
+    public class WorkingCalendarService
+    {
+        private readonly HttpClient _client;
+        private readonly string _baseUrl = "http://localhost:5297/api/v1/WorkingCalendars/";
+
+        public WorkingCalendarService()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new(_baseUrl);
+        }
+
+        public WorkingCalendarService(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new(_baseUrl);
+        }
+
+        public async Task<List<WorkingCalendar?>> GetAsync()
+            => await _client.GetFromJsonAsync<List<WorkingCalendar?>>("");
+    }
+}
+
