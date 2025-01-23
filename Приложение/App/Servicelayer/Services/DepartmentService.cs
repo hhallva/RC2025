fbcdbf9e29c7e7d144d00ec14654faa50469f275
Вячеслав <slavvva05@gmail.com>
2025-01-23 18:50:07 +0300

    Update Services
    Code cleanup

--- Приложение/App/Servicelayer/Services/DepartmentService.cs
+++ Приложение/App/Servicelayer/Services/DepartmentService.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using DataLayer.DataContexts;
+﻿using DataLayer.DataContexts;
 using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataLayer.Services
 {
