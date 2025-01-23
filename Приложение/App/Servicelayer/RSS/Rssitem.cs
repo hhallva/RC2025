b3aba69e779889e00707052126a253acb4f94b41
Вячеслав <slavvva05@gmail.com>
2025-01-23 22:09:21 +0300

    Add Rssitem and RssService

---
+++ Приложение/App/Servicelayer/RSS/Rssitem.cs
@@ -0,0 +1,10 @@
+﻿namespace DataLayer.RSS
+{
+    public class RssItem
+    {
+        public required string Title { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
