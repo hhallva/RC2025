7d433a3fc17536d0ccb4dd5aa8609fa6daa514ed
Вячеслав <slavvva05@gmail.com>
2025-01-25 22:35:07 +0300

    Added event display

--- Приложение/App/WebApp/Pages/Index.cshtml.cs
+++ Приложение/App/WebApp/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using DataLayer.DTOs;
 using DataLayer.Models;
 using DataLayer.RSS;
 using DataLayer.Services;
@@ -5,15 +6,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
-    public class IndexModel(RssService rssService, EmployeeService employeeService) : PageModel
+    public class IndexModel(RssService rssService, EmployeeService employeeService, EventsService eventsService) : PageModel
     {
         public List<RssItem> RssItems { get; set; }
         public List<Employee?> Employees { get; set; }
+        public List<EventDto?> Events { get; set; }
 
         public async Task OnGetAsync()
         {
             RssItems = await rssService.GetRssItemsAsync();
             Employees = await employeeService.GetAllAsync();
+            Events = await eventsService.GetAllAsync();
         }
     }
 }
