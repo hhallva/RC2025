5b9169186ead8daf029044d2d765ad04a63ec48a
Вячеслав <slavvva05@gmail.com>
2025-01-25 21:41:47 +0300

    Add Event(ResponseDto/Dto/Servise)
    I've done getting data from an external API

---
+++ Приложение/App/Servicelayer/DTOs/EventDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DataLayer.DTOs
+{
+    public class EventDto
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("headline")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("abstract")]
+        public string Annotation { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Приложение/App/Servicelayer/Services/EventsService.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.DTOs;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace DataLayer.Services
+{
+    public class EventsService
+    {
+        private readonly HttpClient _client;
+        private readonly string _baseUrl = "https://fakenews.squirro.com/news/technology";
+
+        public EventsService()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new System.Uri(_baseUrl);
+        }
+
+        public async Task<List<EventDto>> GetAllAsync(int since = 0, int count = 10) 
+            => (await _client.GetFromJsonAsync<EventsResponseDto>($"?since={since}&count={count}"))?.Events;
+    }
+}
