9719d7c18c5691c7d0dc396314f9304e9a4054db
Вячеслав <slavvva05@gmail.com>
2025-02-11 20:57:19 +0300

    Update EventDto.cs

--- Приложение/App/Servicelayer/DTOs/EventDto.cs
+++ Приложение/App/Servicelayer/DTOs/EventDto.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text;
+using System.Text.Json.Serialization;
 
 namespace DataLayer.DTOs
 {
@@ -11,12 +12,35 @@ namespace DataLayer.DTOs
         public string Title { get; set; }
 
         [JsonPropertyName("abstract")]
-        public string Annotation { get; set; }
+        public string Description { get; set; }
 
         [JsonPropertyName("author")]
         public string Author { get; set; }
 
         [JsonPropertyName("date")]
         public DateTime Date { get; set; }
+
+        public string IcsData
+        {
+            get
+            {
+                StringBuilder vCard = new();
+                vCard.AppendLine($"BEGIN:VCALENDAR");
+                vCard.AppendLine($"VERSION:2.0");
+                vCard.AppendLine($"BEGIN:VEVENT");
+                vCard.AppendLine($"SUMMARY:{Title}");
+                vCard.AppendLine($"DTSTART:{Date}");
+                vCard.AppendLine($"UID:{Id}");
+                vCard.AppendLine($"DESCRIPTION:{Description}");
+                vCard.AppendLine($"ORGANIZER:{Author}");
+                vCard.AppendLine($"STATUS:CONFIRMED");
+                vCard.AppendLine($"PRIORITY:0");
+                vCard.AppendLine($"END:VEVENT");
+                vCard.AppendLine($"END:VCALENDAR");
+
+                return vCard.ToString();
+            }
+        }
+
     }
 }
