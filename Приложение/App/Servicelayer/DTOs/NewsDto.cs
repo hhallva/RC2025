c43d682fdc523c4c12d88180f34d7d67b122f6d8
Вячеслав <slavvva05@gmail.com>
2025-02-11 20:51:27 +0300

    Chenge RssService, RssItem

---
+++ Приложение/App/Servicelayer/DTOs/NewsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DataLayer.DTOs
+{
+    public class NewsDto
+    {
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Image { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+    }
+}
---
+++ Приложение/App/Servicelayer/Services/NewsService.cs
@@ -0,0 +1,32 @@
+﻿using DataLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataLayer.Services
+{
+    public class NewsService(HttpClient client)
+    {
+        private readonly string url = "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml";
+
+        public async Task<List<NewsDto>> GetAllAsync()
+        {
+            var response = await client.GetStringAsync(url);
+            response = response.Replace("media:content", "image");
+
+            var doc = XDocument.Parse(response);
+            return doc.Descendants("item")
+                .Select(item => new NewsDto()
+                {
+                    Title = (string)item.Element("title"),
+                    Image = (string)item.Element("image")?.Attribute("url"),
+                    Description = (string)item.Element("description"),
+                    PublicationDate = DateTime.Parse((string)item.Element("pubDate"))
+                }).ToList();
+
+        }
+    }
+}
